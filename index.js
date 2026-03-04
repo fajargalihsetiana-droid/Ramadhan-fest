0b4e3ad2281a01d3513266ac380094dc0be818be
fajargalihsetiana-droid <fajargalihsetiana@gmail.com>
2026-03-04 14:09:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -415,219 +415,111 @@ scheduleNext();
 
 client.on("interactionCreate",async interaction=>{
 
-/* ================= BUTTON ================= */
-
 if(interaction.isButton()){
 
-/* ================= BOSS BUTTON ================= */
-
-if(boss){
-
-await interaction.deferReply({ephemeral:true})
-
-/* ATTACK */
-
-if(interaction.customId==="boss_attack"){
-
-let dmg=calculateDamage("attack")
-
-boss.hp-=dmg
-
-if(!bossPlayers[interaction.user.id])
-bossPlayers[interaction.user.id]=0
-
-bossPlayers[interaction.user.id]+=dmg
-
-await updateBoss()
-
-return interaction.editReply({
-content:`⚔️ Kamu menyerang boss!\nDamage: ${dmg}`
-})
-
-}
-
-/* SKILL */
-
-if(interaction.customId==="boss_skill"){
-
-let dmg=calculateDamage("skill")
-
-boss.hp-=dmg
-
-if(!bossPlayers[interaction.user.id])
-bossPlayers[interaction.user.id]=0
-
-bossPlayers[interaction.user.id]+=dmg
-
-await updateBoss()
-
-return interaction.editReply({
-content:`✨ Skill mengenai boss!\nDamage: ${dmg}`
-})
-
-}
-
-/* DEFEND */
-
-if(interaction.customId==="boss_defend"){
-
-return interaction.editReply({
-content:"🛡️ Defense aktif!"
-})
-
-}
-
-/* RAGE MODE */
-
-if(boss.hp<=boss.maxHp/2 && !boss.rage){
-
-boss.rage=true
-
-const channel=interaction.guild.channels.cache.get(process.env.BOSS_CHANNEL_ID)
-
-channel.send("🔥 **BOSS MASUK RAGE MODE!**")
-
-}
-
-/* BOSS DEAD */
-
-if(boss.hp<=0){
-
-boss.hp=0
-
-await updateBoss()
-
-await bossDead(interaction.guild)
-
-}
-
-return
-
-}
-
-/* ================= QUIZ BUTTON ================= */
-
 if(!activeQuiz)
-return interaction.reply({content:"⚠️ Soal selesai.",ephemeral:true})
+return interaction.reply({content:"⚠️ Soal selesai.",ephemeral:true});
 
 if(activeQuiz.answered.includes(interaction.user.id))
-return interaction.reply({content:"❌ Kamu sudah menjawab.",ephemeral:true})
+return interaction.reply({content:"❌ Kamu sudah menjawab.",ephemeral:true});
 
-activeQuiz.answered.push(interaction.user.id)
+activeQuiz.answered.push(interaction.user.id);
 
 if(parseInt(interaction.customId)===activeQuiz.correct){
 
-const user=getUser(interaction.user.id)
+const user=getUser(interaction.user.id);
 
-let reward=Math.floor(Math.random()*11)+40
+let reward=Math.floor(Math.random()*11)+40;
 
 const sorted=Object.entries(data)
-.sort((a,b)=>b[1].points-a[1].points)
+.sort((a,b)=>b[1].points-a[1].points);
 
-const rankIndex=sorted.findIndex(e=>e[0]===interaction.user.id)
+const rankIndex=sorted.findIndex(e=>e[0]===interaction.user.id);
 
 if(rankIndex>=2){
-reward=Math.floor(reward*1.7)
+reward=Math.floor(reward*1.7);
 }
 
 if(!activeQuiz.firstWinner){
-reward*=2
-activeQuiz.firstWinner=interaction.user.id
+reward*=2;
+activeQuiz.firstWinner=interaction.user.id;
 }
 
-reward=applyGapBalance(interaction.user.id,reward)
+reward=applyGapBalance(interaction.user.id,reward);
 
-user.points+=reward
+user.points+=reward;
 
 activeQuiz.winners.push({
 id:interaction.user.id,
 points:reward
-})
+});
 
-saveData()
+saveData();
 
-await updateLeaderboard(interaction.guild)
+await updateLeaderboard(interaction.guild);
 
-await logPoint(interaction.guild,interaction.user.id,reward,"Quiz")
+await logPoint(interaction.guild,interaction.user.id,reward,"Quiz");
 
 return interaction.reply({
 content:`🔥 Benar!\n✨ +${reward} poin\n🏆 Total: ${user.points}`,
 ephemeral:true
-})
+});
 
 }
 
-return interaction.reply({content:"❌ Salah!",ephemeral:true})
+return interaction.reply({content:"❌ Salah!",ephemeral:true});
 
 }
 
-/* ================= SLASH COMMAND ================= */
-
-if(!interaction.isChatInputCommand()) return
-
-/* COOLDOWN */
-
+if(!interaction.isChatInputCommand()) return;
 if(interaction.commandName==="cooldown"){
 
-const user=getUser(interaction.user.id)
-const now=Date.now()
+const user = getUser(interaction.user.id);
+const now = Date.now();
 
-let text="🌙 **STATUS COOLDOWN** 🌙\n"
-text+="━━━━━━━━━━━━━━━━━━\n\n"
+let text = "🌙 **STATUS COOLDOWN** 🌙\n";
+text += "━━━━━━━━━━━━━━━━━━\n\n";
 
 for(const key in keywordCooldown){
 
-const cd=user.keywordCooldowns[key]||0
+const cd = user.keywordCooldowns[key] || 0;
 
-if(now>=cd){
-text+=`✨ **${key.toUpperCase()}** : 🟢 Siap digunakan\n`
+if(now >= cd){
+
+text += `✨ **${key.toUpperCase()}** : 🟢 Siap digunakan\n`;
+
 }else{
-const remain=Math.ceil((cd-now)/60000)
-text+=`⏳ **${key.toUpperCase()}** : ${remain} menit lagi\n`
-}
+
+const remain = Math.ceil((cd - now)/60000);
+text += `⏳ **${key.toUpperCase()}** : ${remain} menit lagi\n`;
 
 }
 
-text+="\n━━━━━━━━━━━━━━━━━━"
-text+=`\n🏆 Total Poin Kamu: **${user.points} poin**`
+}
+
+text += "\n━━━━━━━━━━━━━━━━━━";
+text += `\n🏆 Total Poin Kamu: **${user.points} poin**`;
 
 return interaction.reply({
 content:text,
 ephemeral:true
-})
+});
 
 }
 
-/* SOAL */
-
 if(interaction.commandName==="soal"){
 
-await sendQuiz(interaction.guild)
+await sendQuiz(interaction.guild);
 
 return interaction.reply({
 content:"📢 Quiz dikirim!",
 ephemeral:true
-})
+});
 
 }
 
-/* SPAWN BOSS */
+});
 
-if(interaction.commandName==="spawnboss"){
-
-if(interaction.user.id!==OWNER_ID)
-return interaction.reply({content:"❌ Hanya owner.",ephemeral:true})
-
-await spawnBoss(interaction.guild)
-
-return interaction.reply({
-content:"🐉 Boss berhasil di spawn!",
-ephemeral:true
-})
-
-}
-
-})
 /* ================= READY ================= */
 
 client.once("clientReady",async()=>{
@@ -640,314 +532,6 @@ if(guild){
 startAutoQuizSystem(guild);
 }
 
-/* ================= COMMAND ================= */
-
-const commands=[
-
-new SlashCommandBuilder()
-.setName("cooldown")
-.setDescription("Cek cooldown"),
-
-new SlashCommandBuilder()
-.setName("soal")
-.setDescription("Kirim quiz"),
-
-new SlashCommandBuilder()
-.setName("spawnboss")
-.setDescription("Spawn boss ramadhan"),
-
-new SlashCommandBuilder()
-.setName("addpoin")
-.setDescription("Tambah poin")
-.addUserOption(o=>o.setName("user").setDescription("User").setRequired(true))
-.addIntegerOption(o=>o.setName("jumlah").setDescription("Jumlah").setRequired(true))
-
-].map(c=>c.toJSON());
-
-const rest=new REST({version:"10"}).setToken(process.env.TOKEN);
-
-await rest.put(
-Routes.applicationGuildCommands(client.user.id,process.env.GUILD_ID),
-{body:commands}
-);
-
-console.log("✅ Slash command terdaftar");
-
 });
 
 client.login(process.env.TOKEN);
-
-/* =====================================================
-🐉 RAMADHAN BOSS RAID SYSTEM
-===================================================== */
-
-let boss=null
-let bossMessage=null
-let bossPlayers={}
-
-const BOSS_MAX_HP=5000
-
-/* ================= HP BAR ================= */
-
-function bossHPBar(current,max){
-
-const size=20
-const percent=current/max
-
-const filled=Math.round(size*percent)
-const empty=size-filled
-
-const bar="🟥".repeat(filled)+"⬛".repeat(empty)
-
-const percentText=Math.floor(percent*100)
-
-return `${bar}\n${percentText}%`
-
-}
-
-/* ================= SPAWN BOSS ================= */
-
-async function spawnBoss(guild){
-
-if(boss) return
-
-const channel=guild.channels.cache.get(process.env.BOSS_CHANNEL_ID)
-if(!channel) return
-
-bossPlayers={}
-
-boss={
-hp:BOSS_MAX_HP,
-maxHp:BOSS_MAX_HP,
-rage:false
-}
-
-const embed=new EmbedBuilder()
-
-.setTitle("🐉 BOSS RAMADHAN MUNCUL!")
-
-.setDescription(`
-HP: **${boss.hp} / ${boss.maxHp}**
-
-${bossHPBar(boss.hp,boss.maxHp)}
-
-⚔️ Attack
-🛡️ Defend
-✨ Skill
-`)
-
-.setColor("Red")
-
-.setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4uXZL1cmXfBxKQINQIT1R4H2Orko-9uRJXbmEv7XO9BhrMnxo0AlFnvrW&s=10")
-
-const row=new ActionRowBuilder().addComponents(
-
-new ButtonBuilder()
-.setCustomId("boss_attack")
-.setLabel("⚔️ Attack")
-.setStyle(ButtonStyle.Danger),
-
-new ButtonBuilder()
-.setCustomId("boss_defend")
-.setLabel("🛡️ Defend")
-.setStyle(ButtonStyle.Secondary),
-
-new ButtonBuilder()
-.setCustomId("boss_skill")
-.setLabel("✨ Skill")
-.setStyle(ButtonStyle.Primary)
-
-)
-
-bossMessage=await channel.send({
-
-content:`<@&${process.env.GIVEAWAY_ROLE_ID}> 🐉 **BOSS RAMADHAN MUNCUL!**`,
-
-embeds:[embed],
-components:[row]
-
-})
-
-}
-
-/* ================= UPDATE BOSS ================= */
-
-async function updateBoss(){
-
-if(!bossMessage) return
-
-const sorted=Object.entries(bossPlayers)
-
-.sort((a,b)=>b[1]-a[1])
-.slice(0,5)
-
-let leaderboard=""
-
-sorted.forEach((p,i)=>{
-leaderboard+=`${i+1}. <@${p[0]}> — ${p[1]} dmg\n`
-})
-
-if(!leaderboard) leaderboard="Belum ada serangan"
-
-let rageText=""
-
-if(boss.rage) rageText="🔥 **RAGE MODE AKTIF!**\n"
-
-const embed=new EmbedBuilder()
-
-.setTitle("🐉 RAMADHAN BOSS RAID")
-
-.setDescription(`
-HP: **${boss.hp} / ${boss.maxHp}**
-
-${bossHPBar(boss.hp,boss.maxHp)}
-
-${rageText}
-
-🏆 Top Damage
-
-${leaderboard}
-
-⚔️ Attack
-🛡️ Defend
-✨ Skill
-`)
-
-.setColor("Red")
-
-.setImage("https://cdn.discordapp.com/attachments/1478602183032311919/1478612467201609771/file_000000007da872088dcfe415d2d39f88.png?ex=69a908ab&is=69a7b72b&hm=bc97d7776984994166cdab770f5d965067d895a54ff8fe86499f25e5acce65f7&")
-
-await bossMessage.edit({embeds:[embed]})
-
-}
-
-/* ================= BOSS DEAD ================= */
-
-async function bossDead(guild){
-
-const channel=guild.channels.cache.get(process.env.BOSS_CHANNEL_ID)
-
-const sorted=Object.entries(bossPlayers)
-
-.sort((a,b)=>b[1]-a[1])
-
-let result="🎉 **BOSS RAMADHAN DIKALAHKAN!**\n\n"
-
-sorted.slice(0,5).forEach((p,i)=>{
-result+=`${i+1}. <@${p[0]}> — ${p[1]} damage\n`
-})
-
-channel.send(result)
-
-/* reward */
-
-sorted.forEach((p,i)=>{
-
-const user=getUser(p[0])
-
-if(i===0) user.points+=120
-else if(i===1) user.points+=80
-else if(i===2) user.points+=60
-else user.points+=30
-
-})
-
-saveData()
-
-boss=null
-bossMessage=null
-bossPlayers={}
-
-}
-
-/* ================= DAMAGE ================= */
-
-function calculateDamage(type){
-
-let dmg=0
-
-if(type==="attack") dmg=Math.floor(Math.random()*40)+40
-
-if(type==="skill") dmg=Math.floor(Math.random()*80)+120
-
-/* critical */
-
-if(Math.random()<0.1){
-dmg*=2
-}
-
-if(boss.rage){
-dmg=Math.floor(dmg*1.2)
-}
-
-return dmg
-
-}
-
-/* ================= BUTTON ================= */
-
-client.on("interactionCreate",async interaction=>{
-
-if(!interaction.isButton()) return
-if(!boss) return
-
-/* ATTACK */
-
-if(interaction.customId==="boss_attack"){
-
-let dmg=calculateDamage("attack")
-
-boss.hp-=dmg
-
-if(!bossPlayers[interaction.user.id])
-bossPlayers[interaction.user.id]=0
-
-bossPlayers[interaction.user.id]+=dmg
-
-}
-
-/* SKILL */
-
-if(interaction.customId==="boss_skill"){
-
-let dmg=calculateDamage("skill")
-
-boss.hp-=dmg
-
-if(!bossPlayers[interaction.user.id])
-bossPlayers[interaction.user.id]=0
-
-bossPlayers[interaction.user.id]+=dmg
-
-}
-
-/* DEFEND */
-
-if(interaction.customId==="boss_defend"){
-
-return interaction.reply({
-content:"🛡️ Defense aktif!",
-ephemeral:true
-})
-
-}
-
-/* rage */
-
-if(boss.hp<=boss.maxHp/2 && !boss.rage){
-
-boss.rage=true
-
-const channel=interaction.guild.channels.cache.get(process.env.BOSS_CHANNEL_ID)
-
-channel.send("🔥 **BOSS MASUK RAGE MODE!**")
-
-}
-
-if(boss.hp<=0){
-
-boss.hp=0
-
-await updateBoss()
-
-await
